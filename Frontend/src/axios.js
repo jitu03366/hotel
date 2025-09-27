b19ec793b74253fe6fc9dfee6dfea01071bff879
jitu03366 <jitusharmaji10@gmail.com>
2025-09-28 01:04:22 +0530

    Update axios.js

--- Frontend/src/axios.js
+++ Frontend/src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   // baseURL: "https://hotels-reservation-system.herokuapp.com",
-  baseURL: "http://localhost:4000",
+  baseURL: "https://hotel-79fd.onrender.com",
 });
 
 // Attach Authorization header automatically if token exists
